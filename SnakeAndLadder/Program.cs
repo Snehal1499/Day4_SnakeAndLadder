930b484ba4b190427817b8509a44c2928ee7a619
Snehal1499 <snehalpatil1718@gmail.com>
2022-03-11 15:49:32 +0530

    Added Roll The Die

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-//UC1 StartPosition
+//UC2_RollTheDie
 namespace SnakeAndLadder
 {
     internal class Program
@@ -7,9 +7,14 @@ namespace SnakeAndLadder
         static void Main(string[] args)
         {
             int PlayerPosition = 0;
+            //it is using to genetate random numbers
+            Random random = new Random();
+            int die = random.Next(0, 7);
+
             {
                 Console.WriteLine("welcome to Snake And Ladder Programe");
                 Console.WriteLine("Starting Position is " + PlayerPosition);
+                Console.WriteLine("Dice roll: " + die);
 
             }
         }
